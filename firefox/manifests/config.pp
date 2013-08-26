ca99a334bccdb17fd330ed1ea703f2ce99e1d47b
Javier calles <javiercalles@gmail.com>
2013-08-26 17:18:39 -0430

    puppet module firefox

---
+++ firefox/manifests/config.pp
@@ -0,0 +1,76 @@
+# Class: Firefox Preferences
+#
+# This module manages: User Preferences 
+#
+# Parameters:
+#
+# Actions: File Control firefox.js and firefox.cfg
+#
+# Requires: Repositories
+#
+# Sample Usage: change name jcalles-firefox-0.0.1 to firefox and put on /etc/puppetlabs/puppet/modules/ 
+#               include this module on any node 
+#    node  "fqdn" inherits   {
+#      include firefox
+#                 }
+#
+#
+# [Remember: No empty lines between comments and class definition]
+class firefox::config {
+	case $operatingsystem {
+      	/(CentOS|Redhat)/: { $pathfirefox = "/usr/lib64/firefox/defaults/preferences" }
+      	/(Ubuntu|Debian)/: { $pathfirefox = "/usr/lib/firefox/defaults/pref" }
+      	default: { fail("Unrecognized operating system for Firefox") }
+}
+
+	package {
+	'firefox':
+	ensure => installed,
+}
+	file {
+	'firefox.cfg':
+	path => "$pathfirefox/firefox.cfg",
+	content => template('firefox/firefox.cfg.erb'),
+	mode => 644,
+	owner => 'root',
+	group => 'root',
+	require => Package['firefox'],
+}
+
+	file {
+	'firefox.js':
+	path => "$pathfirefox/firefox.js",
+	source => 'puppet:///modules/firefox/firefox.js',
+	mode => 644,
+	owner => 'root',
+	group => 'root',
+	require => Package['firefox'],
+}
+	file {'/etc/firefox/':
+	ensure => 'directory',
+	group  => 'root',
+	recurse => true,
+	force => true,
+	mode   => '775',
+	owner  => 'root',
+	require => Package['firefox'],
+}
+	file {'/etc/firefox/profile/':
+	ensure => 'directory',
+	group  => 'root',
+	recurse => true,
+	force => true,
+	mode   => '775',
+	owner  => 'root',
+	require => [File['/etc/firefox/'],Package['firefox']],
+}
+	file {
+	'bookmarks.html':
+	path => '/etc/firefox/profile/bookmarks.html',
+	source => 'puppet:///modules/firefox/bookmarks.html',
+	mode => 775,
+	owner => 'root',
+	group => 'root',
+	require => [File['/etc/firefox/profile/'],Package['firefox']],
+}
+}
---
+++ firefox/manifests/init.pp
@@ -0,0 +1,22 @@
+# Class: localusers
+#
+# This module manages localusers
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class firefox {
+
+####### Set ipdddress or fqdn #############################
+$homepage='FQDN/IP'
+
+include firefox::config
+
+
+}
