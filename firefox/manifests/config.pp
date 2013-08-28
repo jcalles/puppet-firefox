bcb78bef5799175de737f6f2c4319c2b6565b59f
Javier calles <javiercalles@gmail.com>
2013-08-28 12:17:24 -0430

    puppet module firefox update fix

--- firefox/manifests/config.pp
+++ firefox/manifests/config.pp
@@ -17,7 +17,6 @@
 #
 # [Remember: No empty lines between comments and class definition]
 class firefox::config {
-<<<<<<< HEAD
 	case $::operatingsystem {
       	/(CentOS|Redhat)/: { 
 		$pathfirefox = "/usr/lib64/firefox/" 
@@ -25,11 +24,6 @@ class firefox::config {
       	/(Ubuntu|Debian)/: { 
 		$pathfirefox = "/usr/lib/firefox/" 
 		$pathpref = "${pathfirefox}/defaults/pref" }
-=======
-	case $operatingsystem {
-      	/(CentOS|Redhat)/: { $pathfirefox = "/usr/lib64/firefox/defaults/preferences" }
-      	/(Ubuntu|Debian)/: { $pathfirefox = "/usr/lib/firefox/defaults/pref" }
->>>>>>> 0628687adc6a48f7f5e9f112dc3240b3e86006a6
       	default: { fail("Unrecognized operating system for Firefox") }
 }
 
@@ -39,11 +33,7 @@ class firefox::config {
 }
 	file {
 	'firefox.cfg':
-<<<<<<< HEAD
 	path => "${pathfirefox}/firefox.cfg",
-=======
-	path => "$pathfirefox/firefox.cfg",
->>>>>>> 0628687adc6a48f7f5e9f112dc3240b3e86006a6
 	content => template('firefox/firefox.cfg.erb'),
 	mode => 644,
 	owner => 'root',
@@ -53,11 +43,7 @@ class firefox::config {
 
 	file {
 	'firefox.js':
-<<<<<<< HEAD
 	path => "${pathpref}/firefox.js",
-=======
-	path => "$pathfirefox/firefox.js",
->>>>>>> 0628687adc6a48f7f5e9f112dc3240b3e86006a6
 	source => 'puppet:///modules/firefox/firefox.js',
 	mode => 644,
 	owner => 'root',
@@ -85,11 +71,7 @@ class firefox::config {
 	file {
 	'bookmarks.html':
 	path => '/etc/firefox/profile/bookmarks.html',
-<<<<<<< HEAD
 	source => 'puppet:///modules/firefox/bookmarks-all.html',
-=======
-	source => 'puppet:///modules/firefox/bookmarks.html',
->>>>>>> 0628687adc6a48f7f5e9f112dc3240b3e86006a6
 	mode => 775,
 	owner => 'root',
 	group => 'root',
--- firefox/manifests/init.pp
+++ firefox/manifests/init.pp
@@ -12,17 +12,7 @@
 #
 # [Remember: No empty lines between comments and class definition]
 class firefox {
-<<<<<<< HEAD
 ####### Set ipdddress or fqdn #############################
 $homepage='FQDN/IP'
 include firefox::config
-=======
-
-####### Set ipdddress or fqdn #############################
-$homepage='FQDN/IP'
-
-include firefox::config
-
-
->>>>>>> 0628687adc6a48f7f5e9f112dc3240b3e86006a6
 }
