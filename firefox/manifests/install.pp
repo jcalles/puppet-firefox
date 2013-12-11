46895a94b735d11e4cbf1b3fe57d16cde1dbe3b7
rexcze <rexcze@gmail.com>
2013-12-11 08:06:24 +0100

    Update install.pp
    change firefox version

--- firefox/manifests/install.pp
+++ firefox/manifests/install.pp
@@ -1,6 +1,6 @@
 class firefox::install {
 case $operatingsystem {
-        /(CentOS|RedHat)/: { $firefox = "firefox-17.0.7-1.el6.centos.x86_64" }
+        /(CentOS|RedHat)/: { $firefox = "firefox" }
         /(Debian|Ubuntu)/: { $firefox = "firefox" }
         default: { fail("Unrecognized operating system for fastesmirror") }
 }
